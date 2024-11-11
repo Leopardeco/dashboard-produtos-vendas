34ea03c66fb0dea3285a1d8ca9da70e95667203a
Leonardo Amaral <leeonardoolliveira1@gmail.com>
2024-11-11 02:19:01 -0300

    mobile finalizado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+import { Sidebar } from "@/components/sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,8 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn("min-h-screen bg-background fount-sans antialiased",
+          geistSans.className
+        )}>
+          <Sidebar/>
         {children}
       </body>
     </html>
---
+++ lib/utils.ts
@@ -0,0 +1,6 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs)) /*Faz renderizações condicionais dentro dos estilos*/
+}
